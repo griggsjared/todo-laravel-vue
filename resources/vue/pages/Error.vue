a4ae0259382dd51009038a86f94dfdab7fda39a5
Jared Griggs <griggsjared@gmail.com>
2022-10-27 22:10:20 -0600

    Added error page

---
+++ resources/vue/pages/Error.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+  import { computed } from 'vue';
+
+  type TError = {
+    title: string;
+  };
+
+  const errorTypes: {
+    [key: number]: TError;
+  } = {
+    503: { title: 'Service Unavailable' },
+    500: { title: "Sorry, we've encountered an error." },
+    404: { title: 'Page Not Found' },
+    401: { title: 'Unauthorized' },
+    403: { title: 'Unauthorized' },
+  };
+
+  const props = defineProps<{
+    status: number;
+  }>();
+
+  const error = computed<TError>(function () {
+    return typeof errorTypes[props.status] !== 'undefined'
+      ? errorTypes[props.status]
+      : { title: `${props.status}: Oops`, links: true };
+  });
+</script>
+
+<template>
+  <div class="flex flex-col min-h-full pt-16 pb-12 bg-blue-dark">
+    <main
+      class="flex flex-col justify-center flex-grow w-full px-4 mx-auto max-w-7xl sm:px-6 lg:px-8"
+    >
+      <div class="text-center">
+        <h1
+          class="mt-2 text-4xl font-extrabold tracking-wide text-white text-gray-900 sm:text-5xl"
+        >
+          {{ error.title }}
+        </h1>
+      </div>
+    </main>
+  </div>
+</template>
