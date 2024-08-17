60ecfdbdad78f0bcfedb722c63212aeb80ba3461
Jared Griggs <griggsjared@gmail.com>
2024-08-17 16:25:27 -0600

    add db:refresh artisan command

---
+++ app/Console/Commands/RefreshDatabaseCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshDatabaseCommand extends Command
+{
+    protected $signature = 'db:refresh';
+
+    protected $description = 'Refresh the database';
+
+    public function handle()
+    {
+        $this->call('migrate:refresh', [
+            '--seed' => true,
+            '--force' => true,
+        ]);
+    }
+}
